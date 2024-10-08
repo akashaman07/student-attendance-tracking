c52e03f8dfae789b1a04214f37ee3a13bb04493b
akashaman07 <akashaman0306@gmail.com>
2024-10-08 13:06:35 +0530

    add kinde Oauth redirect

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import Image from "next/image";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { useEffect } from "react/cjs/react.production.min";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  useEffect(()=>{
+    redirect('/api/auth/login?post_login_redirect_url=/dashboard')
+  },[])
   return (
     <div>
-      <Button>subscribe</Button>
       
     </div>
   );
