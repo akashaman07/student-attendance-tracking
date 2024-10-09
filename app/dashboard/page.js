12e4c75041ed3d8cab2a57995a67b0f8b854c609
akashaman07 <akashaman0306@gmail.com>
2024-10-09 17:05:54 +0530

    add ThemeProvider with the help of shadcn-ui

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,14 @@
+"use client"
 import React from 'react'
+import { useTheme } from 'next-themes'
+import { useEffect } from 'react'
 
 function dashboard() {
+    const { setTheme } = useTheme()
+
+    useEffect(()=>{
+        setTheme('system');
+    },[])
   return (
     <div>dashboard</div>
   )
