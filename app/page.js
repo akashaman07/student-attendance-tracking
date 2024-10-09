3ec5be5b56d7b6767dd51f4e448dce8475733fcb
akashaman07 <akashaman0306@gmail.com>
2024-10-09 16:07:34 +0530

    small change in app/page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import Image from "next/image";
 import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
-import { useEffect } from "react/cjs/react.production.min";
+// import { useEffect } from "react/cjs/react.production.min";
+import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 export default function Home() {
